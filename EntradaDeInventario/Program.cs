89f3d984ec50df20a3efdb7f685f3b300aae3e94
FelixAlbertoM <af301509@gmail.com>
2026-01-16 11:16:55 -0400

    refactor ♻️: coriige

--- EntradaDeInventario/Program.cs
+++ EntradaDeInventario/Program.cs
@@ -43,10 +43,10 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddIdentityCore<ApplicationUser>(options =>
     {
         options.SignIn.RequireConfirmedAccount = false;
-        options.Password.RequireDigit = false;
-        options.Password.RequireLowercase = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
         options.Password.RequiredLength = 6;
     })
     .AddEntityFrameworkStores<Contexto>()
